27fbf242a72011e436aeefe8ae4a46bb7a2932fa
Zhuoyue Wang <zhuoyue2@illinois.edu>
2018-01-31 08:59:08 -0600

    finish

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NamedEntityExtraction
-Temporal Named Entity Extraction
+##  Temporal Named Entity Extraction
+### Zhuoyue Wang, zhuoyue2
+
+Input.txt: the input article we want to extract dates
+Extraction.py: the program we execute
+Output.txt: the file contains date expressions we identified.
+
+How to run:
+
+Be sure in the working directory which contains these files and type "python3 extraction.py" in the terminal. The output will be printed in terminal and also be in "output.txt".
+
+Program Description:
+
+This python program uses regular expression to identify the date expression patterns such as “January 15, 2014", "January 2014", "the 21st of December", "01/15/2014","Monday", "Monday the 23rd", “Christmas", "Labor Day". It only extracts the longest expression to ensure the accuracy.
--- extraction.py
+++ extraction.py
@@ -11,50 +11,73 @@ def textRead(inputName):
     textFile = open(inputName,'r')
     lines = textFile.readlines()
     textFile.close()
-    for line in lines:
-        line = line.lower()
     return lines
 
 
 def check(lines):
-    p1 = re.compile(r'([0-3]\d)?(\s|\/)?((jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)|((1[0-2])|(0\d))|(january|february|march|april|may|june|july|august|september|october|november|december))?(\s|\/)\d{4}?')
     #21 jan 2018 | 02 2017 | jan 2017 | 20 12 2017 | 01/15/2014
-
-    p2 = re.compile(r'((jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)|((1[0-2])|(0\d))|(january|february|march|april|may|june|july|august|september|october|november|december))?(\s|\/)?([0-3]\d)?(\s|\/)\d{4}?')
-    #21 jan 2018 | 02 2017 | jan 2017 | 20 12 2017 |
-    p3 = re.compile(r'((monday|tuesday|wednesday|thursday|friday|saturday|friday)|(mon|tue|wed|thu|fri|sat|sun))?\s(the\s)?([0-3]\d)?(st|nd|rd)?')
-    #monday monday the 23rd
-    p4 = re.compile(r'(the\s)?([0-3]\d(st|nd|rd))?\s(of\s)?(january|february|march|april|may|june|july|august|september|october|november|december)')
+    #jan 21 2018 | 12 2017 | jan 2017 | 20 12 2017
+    #monday the 23rd
     #the 21st of December
-    p5 = re.compile(r'(january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)')
-    # jan january
-    p6 = re.compile(r'(monday|tuesday|wednesday|thursday|friday|saturday|friday|mon|tue|wed|thu|fri|sat|sun)')
-    # monday mon
-    holidays = ["new year's day", "martin luther king day", "easter", "mother's day", "memorial day", "father's day", "independence day",
-    "labor day", "columbus day", "halloween", "veterans day", "thanksgiving day", "christmas" ]
-
-    patternList = [p1, p2, p3, p4, p5, p6, holidays]
+    # jan | january
+    # monday | mon
+    # 2018
+    #New Year
     result = list()
 
     for line in lines:
-        for pattern in patternList:
-            temp = re.findall(pattern,line)
-            if temp != []:
-                for elem in temp:
-                    result.append(elem)
+        pp1 = re.findall(r'(\d\d)(\s|\/)(\d\d|\d)(\s|\/)(\d\d\d\d)', line)
+        for i in pp1:
+            line = line.replace(''.join(i),'')
+        pp21 = re.findall(r'(January|February|March|April|May|June|July|August|September|October|November|December|JANURAY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)(\s|\, |\. )(\d\d|\d)(\s|\, |\. )(\d\d\d\d)', line)
+        for i in pp21:
+            line = line.replace(''.join(i),'')
+        pp22 = re.findall(r'(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(\s|\, |\. )(\d\d|\d)(\s|\, |\. )(\d\d\d\d)', line)
+        for i in pp22:
+            line = line.replace(''.join(i),'')
+        pp23 = re.findall(r'(January|February|March|April|May|June|July|August|September|October|November|December|JANURAY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)(\s|\, |\. )(\d\d\d\d)',line)
+        for i in pp23:
+            line = line.replace(''.join(i),'')
+        pp24 = re.findall(r'(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(\s|\, |\. )(\d\d\d\d)',line)
+        for i in pp24:
+            line = line.replace(''.join(i),'')
+        pp3 = re.findall(r'((Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)|(Mon|Tue|Wed|Thu|Fri|Sat|Sun))\s(the\s)(\d\d|\d)(st|nd|rd|th)', line)
+        for i in pp3:
+            line = line.replace(''.join(i),'')
+        pp4 = re.findall(r'the\s[0-3]\d(st|nd|rd)\sof\s(January|February|March|April|May|June|July|August|September|October|November|December)', line)
+        for i in pp4:
+            line = line.replace(''.join(i),'')
+        pp5 = re.findall(r'(January|February|March|April|May|June|July|August|September|October|November|December|JANURAY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER|Jan |Feb |Mar |Apr |May |Jun |Jul |Aug |Sep |Oct |Nov |Dec |JAN |FEB |MAR |APR |MAY |JUN |JUL |AUG |SEP |OCT |NOV |DEC )', line)
+        for i in pp5:
+            line = line.replace(''.join(i),'')
+        pp6 = re.findall(r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon |Tue |Wed |Thu |Fri |Sat |Sun |MON |TUE |WED |THU |FRI |SAT |SUN )', line)
+        for i in pp6:
+            line = line.replace(''.join(i),'')
+        pp7 = re.findall(r'\d{4}', line)
+        for i in pp7:
+            line = line.replace(''.join(i),'')
+        pp8 = re.findall(r'New Year\'s Day|New Year|NEW YEAR|Martin Luther King Day|Easter|Mother\'s Day|Memorial Day|Father\'s Day|Independence Day|Labor Day|Columbus Day|Halloween|HALLOWEEN|Veterans Day|Thanksgiving Day|Thanksgiving|THANKSGIVING|Christmas|CHRISTMAS', line)
+        for i in pp8:
+            line = line.replace(''.join(i),'')
+        temp = [pp1, pp21, pp22, pp23, pp24, pp3, pp4, pp5, pp6, pp7, pp8]
+        for i in temp:
+            if len(i) != 0:
+                for j in i:
+                    result.append(''.join(j))
+
     return result
 
 
 
 def main():
-    lines = textRead()
+    lines = textRead("input.txt")
     result = check(lines)
+    outputFile = open('output.txt','w')
     for i in result:
-        print("{}\n".format(i))
+        print("{}".format(i))
+        outputFile.write(i)
+        outputFile.write('\n')
+    outputFile.close()
 
 if __name__ == "__main__":
     main()
-
-'''
-
-'''
